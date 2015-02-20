f46009b6a5b255ac5f39707f0ea620642f118f51
nacho <nacho@nacho-Aurora-R4>
2015-02-20 18:39:31 -0500

    added unsupervised

--- classification/python/random_forest.py
+++ classification/python/random_forest.py
@@ -2,6 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 import numpy as np
 
+
 #### READ TRAIN DATA ####
 train_df=pd.read_csv('/media/DATA/datasets/banknoteAuthentication/banknoteTrain.csv', sep=',',header=None)
 train_data = train_df.as_matrix()
@@ -9,6 +10,7 @@ NUM_FEATURES = train_data.shape[1] - 1
 print NUM_FEATURES
 train_features = train_data[:, 0:NUM_FEATURES-1]
 train_targets = train_data[:,NUM_FEATURES]
+
 
 #### TRAIN RANDOM FOREST ####
 clf = RandomForestClassifier(n_estimators=10,n_jobs=8)
@@ -21,9 +23,11 @@ test_data = test_df.as_matrix()
 test_features = test_data[:, 0:NUM_FEATURES-1]
 test_targets = test_data[:,NUM_FEATURES]
 
+
 #### PREDICT ####
 preds = clf.predict(test_features) 
 print clf.feature_importances_
+
 
 #### PERFORMANCE METRICS ####
 conf_matrix = pd.crosstab(preds,test_targets, rownames=["Pred"], colnames=["Actual"])
